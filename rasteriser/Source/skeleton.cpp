e39930be5d133d8dc5be3fa09990f5d6cac8b50b
tomdjackson <tj15918@bristol.ac.uk>
2018-02-22 17:13:43 +0000

    rasteriser not working

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -6,33 +6,47 @@
 #include <stdint.h>
 
 using namespace std;
+using glm::ivec2;
+using glm::vec2;
 using glm::vec3;
 using glm::mat3;
 using glm::vec4;
 using glm::mat4;
 
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 256
+#define SCREEN_WIDTH 300
+#define SCREEN_HEIGHT 300
 #define FULLSCREEN_MODE false
+
+struct Camera{
+  float focalLength;
+  vec4 position;
+};
+Camera camera;
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 void Update();
-void Draw(screen* screen);
+void InitializeStructs();
+void Draw(screen* screen, vector<Triangle>& triangles);
+void VertexShader( const vec4& v, ivec2& p );
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result );
+void DrawLineSDL( screen* screen, ivec2 a, ivec2 b, vec3 color );
+void DrawPolygonEdges( screen* screen, const vector<vec4>& vertices );
 
-int main( int argc, char* argv[] )
-{
-  
+int main( int argc, char* argv[] ){
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
-  
-  while( NoQuitMessageSDL() )
-    {
-      Update();
-      Draw(screen);
-      SDL_Renderframe(screen);
-    }
+  InitializeStructs();
+
+  vector<Triangle> triangles;
+  LoadTestModel(triangles);
+
+  while( NoQuitMessageSDL() ){
+    Update();
+    Draw(screen, triangles);
+    SDL_Renderframe(screen);
+  }
 
   SDL_SaveImage( screen, "screenshot.bmp" );
 
@@ -40,24 +54,75 @@ int main( int argc, char* argv[] )
   return 0;
 }
 
-/*Place your drawing here*/
-void Draw(screen* screen)
-{
-  /* Clear buffer */
-  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
-    }
+void InitializeStructs(){
+  /* Camera */
+  camera.position = vec4( 0, 0, -3.001, 1 );
+  camera.focalLength = 35;
 }
 
-/*Place updates of parameters here*/
-void Update()
-{
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result ){
+  int N = result.size();
+  vec2 step = vec2(b-a) / float(max(N-1,1));
+  vec2 current( a );
+  for( int i=0; i<N; ++i ){
+    result[i] = current;
+    current += step;
+  }
+}
+
+void VertexShader( const vec4& v, ivec2& p ){
+  p.x = ((camera.focalLength * v.x/v.z) + SCREEN_WIDTH/2) - camera.position.x;
+  p.y = ((camera.focalLength * v.y/v.z) + SCREEN_HEIGHT/2) - camera.position.y;
+}
+
+void DrawLineSDL( screen* screen, ivec2 a, ivec2 b, vec3 color ){
+  ivec2 delta = glm::abs( a - b );
+  int pixels = glm::max( delta.x, delta.y ) + 1;
+
+  vector<ivec2> line( pixels );
+  Interpolate(a, b, line);
+
+  for(int i = 0; i<pixels; ++i){
+    PutPixelSDL(screen, line[i].x, line[i].y, color);
+  }
+}
+
+void DrawPolygonEdges( screen* screen, const vector<vec4>& vertices ){
+  int V = vertices.size();
+  // Transform each vertex from 3D world position to 2D image position:
+  vector<ivec2> projectedVertices( V );
+  for( int i=0; i<V; ++i ){
+    VertexShader( vertices[i], projectedVertices[i] );
+  }
+  // Loop over all vertices and draw the edge from it to the next vertex:
+  for( int i=0; i<V; ++i ){
+    int j = (i+1)%V; // The next vertex
+    vec3 color( 1, 1, 1 );
+    DrawLineSDL( screen, projectedVertices[i], projectedVertices[j], color );
+  }
+}
+
+/*Place your drawing here*/
+void Draw(screen* screen, vector<Triangle>& triangles){
+  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
+
+  for( uint32_t i=0; i<triangles.size(); ++i ){
+    vector<vec4> vertices(3);
+    vertices[0] = triangles[i].v0;
+    vertices[1] = triangles[i].v1;
+    vertices[2] = triangles[i].v2;
+
+    for(int i = 0; i<3; i++){
+      ivec2 pos;
+      VertexShader(vertices[i], pos);
+      PutPixelSDL(screen, (float)pos.x, (float)pos.y, vec3(1,1,1));
+    }
+
+    // DrawPolygonEdges(screen, vertices);
+  }
+}
+
+void Update(){
   static int t = SDL_GetTicks();
   /* Compute frame time */
   int t2 = SDL_GetTicks();
