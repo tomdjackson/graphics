e0899350fa984d83af01e66bfb38a576c678c69a
vanshdassani <vanshdassani@gmail.com>
2018-04-09 23:19:59 +0400

    Rotation/Translation mostly working, yaw needs work, Illumination next

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -10,7 +10,6 @@ using glm::vec3;
 using glm::mat3;
 using glm::vec4;
 using glm::mat4;
-using glm::mat4x4;
 using glm::ivec2;
 using glm::vec2;
 
@@ -25,6 +24,7 @@ using glm::vec2;
 struct Camera {
     float focalLength;
     vec4 position;
+    mat4 R;
     float yaw;
 };
 Camera camera;
@@ -35,7 +35,12 @@ struct Pixel {
     float zinv;
 };
 
-float depthBuffer[SCREEN_HEIGHT][SCREEN_WIDTH];
+float depthBuffer[SCREEN_WIDTH][SCREEN_HEIGHT];
+
+mat4 identityMatrix = mat4(1.0f);
+
+const float rotationIncr = 0.1;
+const float translationIncr = 0.5;
 
 /*
  * FUNCTIONS
@@ -45,13 +50,15 @@ void Update();
 
 void Draw(screen *screen, vector <Triangle> & triangles);
 
-//void TransformationMatrix(mat4x4 M);
+void Rotate();
 
 void InitialiseStructs();
 
+void InitialiseDepthBuffer();
+
 void VertexShader(const vec4& v, Pixel& p);
 
-void Interpolate(Pixel a, Pixel b, vector<Pixel>& result);
+void InterpolatePixel(Pixel a, Pixel b, vector<Pixel>& result);
 
 void DrawLineSDL(Pixel a, Pixel b, vec3 color, screen *screen);
 
@@ -84,6 +91,7 @@ int main(int argc, char *argv[]) {
 void Draw(screen* screen, vector<Triangle>&triangles) {
     /* Clear buffer */
     memset(screen->buffer, 0, screen->height * screen->width * sizeof(uint32_t));
+    InitialiseDepthBuffer();
 
     for (uint32_t i = 0; i<triangles.size(); ++i) {
         vector<vec4> vertices(3);
@@ -102,12 +110,67 @@ void Update() {
     static int t = SDL_GetTicks();
     /* Compute frame time */
     int t2 = SDL_GetTicks();
-//  float dt = float(t2 - t);
+    float dt = float(t2 - t);
     t = t2;
 
-//    std::cout << "Render time: " << dt << " ms." << std::endl;
-    /* Update variables*/
+    std::cout << "Render time: " << dt << " ms." << std::endl;
 
+    /* Update view */
+    vec4 right(camera.R[0][0], camera.R[0][1], camera.R[0][2], 1);
+    vec4 up(camera.R[1][0], camera.R[1][1], camera.R[1][2], 1);
+    vec4 forward(camera.R[2][0], camera.R[2][1], camera.R[2][2], 1);
+
+    const Uint8 *keyPress = SDL_GetKeyboardState(0);
+    if (keyPress[SDL_SCANCODE_UP]) {
+        camera.position += translationIncr * up;
+//        Rotate();
+    }
+    if (keyPress[SDL_SCANCODE_DOWN]) {
+        camera.position -= translationIncr * up;
+//        Rotate();
+    }
+    if (keyPress[SDL_SCANCODE_O]) {
+        camera.position += translationIncr * forward;
+//        Rotate();
+    }
+    if (keyPress[SDL_SCANCODE_K]) {
+        camera.position -= translationIncr * forward;
+//        Rotate();
+    }
+    if (keyPress[SDL_SCANCODE_RIGHT]) {
+        camera.position += translationIncr * right;
+//        Rotate();
+    }
+    if (keyPress[SDL_SCANCODE_LEFT]) {
+        camera.position -= translationIncr * right;
+//        Rotate();
+    }
+    // Rotate left
+    if (keyPress[SDL_SCANCODE_Q]) {
+        camera.position -= translationIncr * right;
+        camera.yaw -= rotationIncr;
+        Rotate();
+
+    }
+    // Rotate right
+    if (keyPress[SDL_SCANCODE_E]) {
+        camera.yaw += rotationIncr;
+        Rotate();
+        camera.position += translationIncr * right;
+    }
+    // Reset
+    if (keyPress[SDL_SCANCODE_R]) {
+        camera.position = vec4(0, 0, -3.001, 1);
+        camera.yaw = 0;
+        camera.R = identityMatrix;
+    }
+}
+
+void Rotate() {
+    camera.R[0][0] = cos(camera.yaw);
+    camera.R[0][2] = sin(camera.yaw);
+    camera.R[2][0] = -sin(camera.yaw);
+    camera.R[2][2] = cos(camera.yaw);
 }
 
 void InitialiseStructs() {
@@ -115,15 +178,24 @@ void InitialiseStructs() {
     camera.position = vec4(0, 0, -3.001, 1);
     camera.focalLength = SCREEN_HEIGHT;
     camera.yaw = 0;
+    camera.R = identityMatrix;
 }
 
-void VertexShader(const vec4& v, Pixel& p) {
-    vec4 v_ = v - camera.position;
-    p.x = (camera.focalLength * (v_.x / v_.z)) + (SCREEN_WIDTH / 2);
-    p.y = (camera.focalLength * (v_.y / v_.z)) + (SCREEN_HEIGHT / 2);
+void InitialiseDepthBuffer() {
+    for(int i=0; i < SCREEN_HEIGHT; i++)
+        for(int j=0; j < SCREEN_WIDTH; j++)
+            depthBuffer[j][i] = 0;
 }
 
-void Interpolate(Pixel a, Pixel b, vector<Pixel>& result) {
+void VertexShader(const vec4& v, Pixel& pix) {
+    vec4 point = (v - camera.position) * camera.R;
+
+    pix.zinv = 1/point.z;
+    pix.x = (int) (camera.focalLength * (point.x / point.z)) + (SCREEN_WIDTH / 2);
+    pix.y = (int) (camera.focalLength * (point.y / point.z)) + (SCREEN_HEIGHT / 2);
+}
+
+void InterpolatePixel(Pixel a, Pixel b, vector<Pixel>& result) {
     int N = result.size();
 
     vec3 step((b.x - a.x),(b.y - a.y), (b.zinv - a.zinv));
@@ -143,19 +215,17 @@ void DrawLineSDL(Pixel a, Pixel b, vec3 color, screen *screen) {
     int delta_y = abs(a.y - b.y);
     int pixels = glm::max(delta_x, delta_y) + 1;
     vector<Pixel> line(pixels);
-    Interpolate(a, b, line);
+    InterpolatePixel(a, b, line);
 
     for (int i = 0; i < pixels; i++) {
-        PutPixelSDL(screen, line[i].x, line[i].y, color);
+        int x = line[i].x;
+        int y = line[i].y;
+        if (line[i].zinv > depthBuffer[x][y]) {
+            depthBuffer[x][y] = line[i].zinv;
+            PutPixelSDL(screen, line[i].x, line[i].y, color);
+        }
+
     }
-
-//    ivec2 delta = glm::abs(a - b);
-//    int pixels = glm::max(delta.x, delta.y) + 1;
-//    vector<Pixel> line(pixels);
-//    Interpolate(a, b, line);
-//
-//    for (int i = 0; i<pixels; i++) PutPixelSDL(screen, line[i].x, line[i].y, color);
-
 }
 
 void ComputePolygonRows(const vector<Pixel>& vertexPixels, vector<Pixel>& leftPixels, vector<Pixel>& rightPixels) {
@@ -176,15 +246,14 @@ void ComputePolygonRows(const vector<Pixel>& vertexPixels, vector<Pixel>& leftPi
     for (int i = 0; i < rows; i++) {
         leftPixels[i].x = +numeric_limits<int>::max();
         rightPixels[i].x = -numeric_limits<int>::max();
-        leftPixels[i].y = min_y + i;
-        rightPixels[i].y = leftPixels[i].y;
+        rightPixels[i].y = leftPixels[i].y = min_y + i;
     }
 
     for (int i = 0; i < vPSize; i++) {
         int j = (i + 1) % vPSize;
         int numPix = abs(vertexPixels[i].y - vertexPixels[j].y) + 1;
         vector<Pixel> line(numPix);
-        Interpolate(vertexPixels[i], vertexPixels[j], line);
+        InterpolatePixel(vertexPixels[i], vertexPixels[j], line);
 
         for (int k = 0; k < numPix; k++) {
             int x = line[k].x;
