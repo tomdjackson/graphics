335e19571045586291d8d2c5fd0ccdd0096c7633
tomdjackson <tj15918@bristol.ac.uk>
2018-04-23 12:42:14 +0100

    spheres working

--- raytracer/Source/TestModelH.h
+++ raytracer/Source/TestModelH.h
@@ -5,188 +5,295 @@
 
 #include <glm/glm.hpp>
 #include <vector>
+#include "glm/ext.hpp"
 
 enum Material { Spec, Diff, Glass};
 
-// Used to describe a triangular surface:
-class Triangle
-{
+class Object {
 public:
-	glm::vec4 v0;
-	glm::vec4 v1;
-	glm::vec4 v2;
-	glm::vec4 normal;
-	glm::vec3 color;
-	Material material;
+  glm::vec3 color;
+  Material material;
 
-	Triangle( glm::vec4 v0, glm::vec4 v1, glm::vec4 v2, glm::vec3 color, Material material )
-		: v0(v0), v1(v1), v2(v2), color(color), material(material)
-	{
-		ComputeNormal();
-	}
+  Object(glm::vec3 color, Material material): color(color), material(material){} // constructor
+  virtual ~Object() {} // virtual destructor
+  virtual bool intersect(glm::vec4 orig, glm::vec4 dir, float &t, glm::vec4 &position) const = 0;
+  virtual void scale(float L) = 0;
+  virtual glm::vec4 computeNormal(glm::vec4 position) const = 0;
+};
 
-	void ComputeNormal()
-	{
-	  glm::vec3 e1 = glm::vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
-	  glm::vec3 e2 = glm::vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
-	  glm::vec3 normal3 = glm::normalize( glm::cross( e2, e1 ) );
-	  normal.x = normal3.x;
-	  normal.y = normal3.y;
-	  normal.z = normal3.z;
-	  normal.w = 1.0;
-	}
+bool solveQuadratic(const float &a, const float &b, const float &c, float &x0, float &x1)
+{
+    float discr = b * b - 4 * a * c;
+    if (discr < 0) return false;
+    else if (discr == 0) {
+        x0 = x1 = - 0.5 * b / a;
+    }
+    else {
+        float q = (b > 0) ?
+            -0.5 * (b + sqrt(discr)) :
+            -0.5 * (b - sqrt(discr));
+        x0 = q / a;
+        x1 = c / q;
+    }
+
+    return true;
+}
+
+class Sphere : public Object{
+public:
+  float radius;
+  float radius2;
+  glm::vec4 center;
+
+  Sphere(glm::vec4 c, float r, glm::vec3 color, Material material): Object(color, material), radius(r), radius2(r*r), center(c){}
+
+  bool intersect(glm::vec4 orig, glm::vec4 dir, float &t, glm::vec4 &position) const{
+    float t0, t1;
+    glm::vec4 L = orig - center;
+    float a = glm::dot(dir, dir);
+    float b = 2 * glm::dot(dir, L);
+    float c = glm::dot(L, L) - radius2;
+    if (!solveQuadratic(a, b, c, t0, t1)) return false;
+
+    if (t0 > t1) std::swap(t0, t1); 
+
+    if (t0 < 0) {
+      t0 = t1;
+      if (t0 < 0) return false;
+    }
+    t = t0;
+    position = orig + (dir * t);
+    return true;
+  }
+
+  glm::vec4 computeNormal( glm::vec4 position) const{
+    glm::vec4 normal = position - center;
+    glm::vec3 normal3 = glm::vec3(normal.x, normal.y, normal.z);
+    normal3 = glm::normalize(normal3);
+    normal = glm::vec4(normal3.x, normal3.y, normal3.z, 1.f);
+    return normal;
+  }
+
+  void scale(float L){
+    // center *= L/2;
+    // center -= glm::vec4(1,1,1,1);
+    // center.x *= -1;
+    // center.y *= -1;
+    // center.w = 1.0;
+    //
+    // radius *= L/2;
+    // radius -= 1;
+    // radius *= -1;
+    // radius2 = radius * radius;
+  }
+};
+
+// Used to describe a triangular surface:
+class Triangle : public Object{
+public:
+  glm::vec4 v0;
+  glm::vec4 v1;
+  glm::vec4 v2;
+
+  Triangle( glm::vec4 v0, glm::vec4 v1, glm::vec4 v2, glm::vec3 color, Material material):
+  Object(color, material), v0(v0), v1(v1), v2(v2) {}
+
+  bool intersect(glm::vec4 start, glm::vec4 dir, float &t, glm::vec4 &position) const{
+    bool intersect = false;
+    glm::vec3 e1 = glm::vec3(v1.x - v0.x, v1.y - v0.y, v1.z - v0.z);
+    glm::vec3 e2 = glm::vec3(v2.x - v0.x, v2.y - v0.y, v2.z - v0.z);
+    glm::vec3 b = glm::vec3(start.x - v0.x, start.y - v0.y, start.z - v0.z);
+
+    glm::vec3 d = glm::vec3(dir.x, dir.y, dir.z);
+    glm::mat3 A(-d, e1, e2);
+
+    float detA = glm::determinant(A);
+    float det = glm::determinant(glm::mat3(b, e1, e2));
+    t = det / detA;
+
+    if(t>=0){
+      det = glm::determinant(glm::mat3(-d, b, e2));
+      float u = det / detA;
+      if(u >= 0 ){
+        det = glm::determinant(glm::mat3(-d, e1, b));
+        float v = det/detA;
+        if(v >= 0 && (u + v) <= 1){
+          intersect = true;
+          position = v0 + (u * glm::vec4(e1.x,e1.y,e1.z, 0)) + (v * glm::vec4(e2.x,e2.y,e2.z, 0));
+        }
+      }
+    }
+    return intersect;
+  }
+
+  glm::vec4 computeNormal(glm::vec4 intersect) const  {
+    glm::vec3 e1 = glm::vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
+    glm::vec3 e2 = glm::vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
+    glm::vec3 normal3 = glm::normalize( glm::cross( e2, e1 ) );
+    glm::vec4 normal;
+    normal.x = normal3.x;
+    normal.y = normal3.y;
+    normal.z = normal3.z;
+    normal.w = 1.0;
+    return normal;
+  }
+
+  void scale(float L) {
+    v0 *= 2/L;
+    v1 *= 2/L;
+    v2 *= 2/L;
+
+    v0 -= glm::vec4(1,1,1,1);
+    v1 -= glm::vec4(1,1,1,1);
+    v2 -= glm::vec4(1,1,1,1);
+
+    v0.x *= -1;
+    v1.x *= -1;
+    v2.x *= -1;
+
+    v0.y *= -1;
+    v1.y *= -1;
+    v2.y *= -1;
+
+    v0.w = 1.0;
+    v1.w = 1.0;
+    v2.w = 1.0;
+  }
 };
 
 // Loads the Cornell Box. It is scaled to fill the volume:
 // -1 <= x <= +1
 // -1 <= y <= +1
 // -1 <= z <= +1
-void LoadTestModel( std::vector<Triangle>& triangles )
+void LoadTestModel( std::vector<Object*>& objects )
 {
-	using glm::vec3;
-	using glm::vec4;
+  using glm::vec3;
+  using glm::vec4;
 
-	// Defines colors:
-	vec3 red(    0.75f, 0.15f, 0.15f );
-	vec3 yellow( 0.75f, 0.75f, 0.15f );
-	vec3 green(  0.15f, 0.75f, 0.15f );
-	vec3 cyan(   0.15f, 0.75f, 0.75f );
-	vec3 blue(   0.15f, 0.15f, 0.75f );
-	vec3 purple( 0.75f, 0.15f, 0.75f );
-	vec3 white(  0.75f, 0.75f, 0.75f );
+  // Defines colors:
+  vec3 red(    0.75f, 0.15f, 0.15f );
+  vec3 yellow( 0.75f, 0.75f, 0.15f );
+  vec3 green(  0.15f, 0.75f, 0.15f );
+  vec3 cyan(   0.15f, 0.75f, 0.75f );
+  vec3 blue(   0.15f, 0.15f, 0.75f );
+  vec3 purple( 0.75f, 0.15f, 0.75f );
+  vec3 white(  0.75f, 0.75f, 0.75f );
 
-	triangles.clear();
-	triangles.reserve( 5*2*3 );
+  objects.clear();
+  objects.reserve( 5*2*3 );
 
-	// ---------------------------------------------------------------------------
-	// Room
+  // ---------------------------------------------------------------------------
+  // Room
 
-	float L = 555;			// Length of Cornell Box side.
+  float L = 555;			// Length of Cornell Box side.
 
-	vec4 A(L,0,0,1);
-	vec4 B(0,0,0,1);
-	vec4 C(L,0,L,1);
-	vec4 D(0,0,L,1);
+  vec4 A(L,0,0,1);
+  vec4 B(0,0,0,1);
+  vec4 C(L,0,L,1);
+  vec4 D(0,0,L,1);
 
-	vec4 E(L,L,0,1);
-	vec4 F(0,L,0,1);
-	vec4 G(L,L,L,1);
-	vec4 H(0,L,L,1);
+  vec4 E(L,L,0,1);
+  vec4 F(0,L,0,1);
+  vec4 G(L,L,L,1);
+  vec4 H(0,L,L,1);
 
-	// Floor:
-	triangles.push_back( Triangle( C, B, A, green, Diff ) );
-	triangles.push_back( Triangle( C, D, B, green, Diff) );
+  //Spheres
+  objects.push_back( new Sphere(glm::vec4(-1,0,0,1), 0.35, red, Diff));
 
-	// Left wall
-	triangles.push_back( Triangle( A, E, C, purple, Diff) );
-	triangles.push_back( Triangle( C, E, G, purple, Diff) );
+  // Floor:
+  objects.push_back( new Triangle( C, B, A, green, Diff ));
+  objects.push_back( new Triangle( C, D, B, green, Diff) );
 
-	// Right wall
-	triangles.push_back( Triangle( F, B, D, yellow, Spec) );
-	triangles.push_back( Triangle( H, F, D, yellow, Spec) );
+  // Left wall
+  objects.push_back( new Triangle( A, E, C, purple, Diff));
+  objects.push_back( new Triangle( C, E, G, purple, Diff));
 
-	// Ceiling
-	triangles.push_back( Triangle( E, F, G, cyan, Diff) );
-	triangles.push_back( Triangle( F, H, G, cyan, Diff) );
+  // Right wall
+  objects.push_back( new Triangle( F, B, D, yellow, Spec));
+  objects.push_back( new Triangle( H, F, D, yellow, Spec));
 
-	// Back wall
-	triangles.push_back( Triangle( G, D, C, white, Diff) );
-	triangles.push_back( Triangle( G, H, D, white, Diff) );
+  // Ceiling
+  objects.push_back( new Triangle( E, F, G, cyan, Diff) );
+  objects.push_back( new Triangle( F, H, G, cyan, Diff) );
 
-	// ---------------------------------------------------------------------------
-	// Short block
+  // Back wall
+  objects.push_back( new Triangle( G, D, C, white, Diff) );
+  objects.push_back( new Triangle( G, H, D, white, Diff) );
 
-	A = vec4(290,0,114,1);
-	B = vec4(130,0, 65,1);
-	C = vec4(240,0,272,1);
-	D = vec4( 82,0,225,1);
+  // ---------------------------------------------------------------------------
+  // Short block
 
-	E = vec4(290,165,114,1);
-	F = vec4(130,165, 65,1);
-	G = vec4(240,165,272,1);
-	H = vec4( 82,165,225,1);
+  A = vec4(290,0,114,1);
+  B = vec4(130,0, 65,1);
+  C = vec4(240,0,272,1);
+  D = vec4( 82,0,225,1);
 
-	// Front
-	triangles.push_back( Triangle(E,B,A,red, Glass) );
-	triangles.push_back( Triangle(E,F,B,red, Glass) );
+  E = vec4(290,165,114,1);
+  F = vec4(130,165, 65,1);
+  G = vec4(240,165,272,1);
+  H = vec4( 82,165,225,1);
 
-	// Front
-	triangles.push_back( Triangle(F,D,B,red, Glass) );
-	triangles.push_back( Triangle(F,H,D,red, Glass) );
+  // Front
+  objects.push_back( new Triangle(E,B,A,red, Glass)) ;
+  objects.push_back( new Triangle(E,F,B,red, Glass)) ;
 
-	// BACK
-	triangles.push_back( Triangle(H,C,D,red, Glass) );
-	triangles.push_back( Triangle(H,G,C,red, Glass) );
+  // Front
+  objects.push_back( new Triangle(F,D,B,red, Glass)) ;
+  objects.push_back( new Triangle(F,H,D,red, Glass)) ;
 
-	// LEFT
-	triangles.push_back( Triangle(G,E,C,red, Glass) );
-	triangles.push_back( Triangle(E,A,C,red, Glass) );
+  // BACK
+  objects.push_back( new Triangle(H,C,D,red, Glass)) ;
+  objects.push_back( new Triangle(H,G,C,red, Glass)) ;
 
-	// TOP
-	triangles.push_back( Triangle(G,F,E,red, Glass) );
-	triangles.push_back( Triangle(G,H,F,red, Glass) );
+  // LEFT
+  objects.push_back( new Triangle(G,E,C,red, Glass)) ;
+  objects.push_back( new Triangle(E,A,C,red, Glass)) ;
 
-	// ---------------------------------------------------------------------------
-	// Tall block
+  // TOP
+  objects.push_back( new Triangle(G,F,E,red, Glass)) ;
+  objects.push_back( new Triangle(G,H,F,red, Glass)) ;
 
-	A = vec4(423,0,247,1);
-	B = vec4(265,0,296,1);
-	C = vec4(472,0,406,1);
-	D = vec4(314,0,456,1);
+  // ---------------------------------------------------------------------------
+  // Tall block
 
-	E = vec4(423,330,247,1);
-	F = vec4(265,330,296,1);
-	G = vec4(472,330,406,1);
-	H = vec4(314,330,456,1);
+  A = vec4(423,0,247,1);
+  B = vec4(265,0,296,1);
+  C = vec4(472,0,406,1);
+  D = vec4(314,0,456,1);
 
-	// Front
-	triangles.push_back( Triangle(E,B,A,blue, Diff) );
-	triangles.push_back( Triangle(E,F,B,blue, Diff) );
+  E = vec4(423,330,247,1);
+  F = vec4(265,330,296,1);
+  G = vec4(472,330,406,1);
+  H = vec4(314,330,456,1);
 
-	// Front
-	triangles.push_back( Triangle(F,D,B,blue, Diff) );
-	triangles.push_back( Triangle(F,H,D,blue, Diff) );
+  // Front
+  objects.push_back( new Triangle(E,B,A,blue, Diff) );
+  objects.push_back( new Triangle(E,F,B,blue, Diff) );
 
-	// BACK
-	triangles.push_back( Triangle(H,C,D,blue, Diff) );
-	triangles.push_back( Triangle(H,G,C,blue, Diff) );
+  // Front
+  objects.push_back( new Triangle(F,D,B,blue, Diff) );
+  objects.push_back( new Triangle(F,H,D,blue, Diff) );
 
-	// LEFT
-	triangles.push_back( Triangle(G,E,C,blue, Diff) );
-	triangles.push_back( Triangle(E,A,C,blue, Diff) );
+  // BACK
+  objects.push_back( new Triangle(H,C,D,blue, Diff) );
+  objects.push_back( new Triangle(H,G,C,blue, Diff) );
 
-	// TOP
-	triangles.push_back( Triangle(G,F,E,blue, Diff) );
-	triangles.push_back( Triangle(G,H,F,blue, Diff) );
+  // LEFT
+  objects.push_back( new Triangle(G,E,C,blue, Diff) );
+  objects.push_back( new Triangle(E,A,C,blue, Diff) );
+
+  // TOP
+  objects.push_back( new Triangle(G,F,E,blue, Diff) );
+  objects.push_back( new Triangle(G,H,F,blue, Diff) );
 
 
-	// ----------------------------------------------
-	// Scale to the volume [-1,1]^3
+  // ----------------------------------------------
+  // Scale to the volume [-1,1]^3
 
-	for( size_t i=0; i<triangles.size(); ++i )
-	{
-		triangles[i].v0 *= 2/L;
-		triangles[i].v1 *= 2/L;
-		triangles[i].v2 *= 2/L;
-
-		triangles[i].v0 -= vec4(1,1,1,1);
-		triangles[i].v1 -= vec4(1,1,1,1);
-		triangles[i].v2 -= vec4(1,1,1,1);
-
-		triangles[i].v0.x *= -1;
-		triangles[i].v1.x *= -1;
-		triangles[i].v2.x *= -1;
-
-		triangles[i].v0.y *= -1;
-		triangles[i].v1.y *= -1;
-		triangles[i].v2.y *= -1;
-
-		triangles[i].v0.w = 1.0;
-		triangles[i].v1.w = 1.0;
-		triangles[i].v2.w = 1.0;
-
-		triangles[i].ComputeNormal();
-	}
+  for( int i=0; i<objects.size(); ++i )
+  {
+    objects[i]->scale(L);
+  }
 }
 
 #endif
--- raytracer/Source/skeleton.cpp
+++ raytracer/Source/skeleton.cpp
@@ -6,26 +6,30 @@
 #include "TestModelH.h"
 #include <stdint.h>
 #include <limits.h>
+#include "glm/ext.hpp"
 
 using namespace std;
+using std::vector;
+using std::unique_ptr;
+using std::ref;
 using glm::vec3;
 using glm::mat3;
 using glm::vec4;
 using glm::mat4;
 
-#define SCREEN_WIDTH 300
-#define SCREEN_HEIGHT 300
+#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 500
 #define FULLSCREEN_MODE false
 #define PI 3.14159265
 
 /* TODO: Extension ideas
 * 1. Global Illumination
-* 2. Spheres
+* 2. Phong shading
 * 3. General Models
 * 4. Textures
 * 5. Parallelism
 * 6. anti-aliasing
-* 7. Phong shading
+* 7. water
 */
 
 /* Extensions DONE
@@ -35,6 +39,7 @@ using glm::mat4;
 * 4. Cramer's Rule
 * 5. Depth of Field
 * 6. Fresnel Effect ---- Needs Checking? Looks a bit strange
+* 7. Spheres
 */
 
 /* ----------------------------------------------------------------------------*/
@@ -59,7 +64,7 @@ Camera camera;
 struct Intersection{
   vec4 position;
   float distance;
-  int triangleIndex;
+  int objectIndex;
 };
 
 struct Options{
@@ -69,21 +74,23 @@ struct Options{
 };
 Options options;
 
+vector<Object*> objects;
+
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 void InitializeStructs();
 void Update();
-void Draw(screen* screen, const vector<Triangle>& triangles);
-bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection );
-vec3 DirectLight( const Intersection& i, const vector<Triangle>& triangles );
-vec3 get_color(const Intersection& i, const vector<Triangle>& triangles, vec4 dir, int depth);
+void Draw(screen* screen);
+bool ClosestIntersection(vec4 start, vec4 dir, Intersection& closestIntersection );
+vec3 DirectLight( const Intersection& i);
+vec3 get_color(const Intersection& i, vec4 dir, int depth);
 vec4 reflect(vec4 normal, vec4 dir);
 vec4 refract(vec4 normal, vec4 dir, float& n, float& k);
 float fresnel(vec4 normal, vec4 dir, const Intersection& i, float& n, float& k);
 mat4 y_rotation(float deg);
-vector<vec4> RandCameraPoints();
-vector<vec4> RandLightPoints();
+void RandCameraPoints(vector<vec4> &points);
+void RandLightPoints(vector<vec4> &points);
 
 /* ----------------------------------------------------------------------------*/
 
@@ -91,12 +98,11 @@ int main( int argc, char* argv[] ){
   InitializeStructs();
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
 
-  vector<Triangle> triangles;
-  LoadTestModel(triangles);
+  LoadTestModel(objects);
 
   while( NoQuitMessageSDL() ){
     Update();
-    Draw(screen, triangles);
+    Draw(screen);
     SDL_Renderframe(screen);
     SDL_SaveImage( screen, "screenshot.png" );
   }
@@ -119,47 +125,30 @@ void InitializeStructs(){
   light.width = 0.2f;
 
   /* Options */
-  options.max_depth = 5;
-  options.num_dof_rays = 2;
+  options.max_depth = 20;
+  options.num_dof_rays = 1;
   options.num_light_rays = 1;
 }
 
-bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection){
+bool ClosestIntersection(vec4 start, vec4 dir, Intersection& closestIntersection){
   bool intersect = false;
   closestIntersection.distance = std::numeric_limits<float>::max();
-
-  for(int i=0; i<triangles.size(); i++){
-    vec3 e1 = vec3(triangles[i].v1.x - triangles[i].v0.x, triangles[i].v1.y - triangles[i].v0.y, triangles[i].v1.z - triangles[i].v0.z);
-    vec3 e2 = vec3(triangles[i].v2.x - triangles[i].v0.x, triangles[i].v2.y - triangles[i].v0.y, triangles[i].v2.z - triangles[i].v0.z);
-    vec3 b = vec3(start.x - triangles[i].v0.x, start.y - triangles[i].v0.y, start.z - triangles[i].v0.z);
-
-    vec3 d = vec3(dir.x, dir.y, dir.z);
-    mat3 A(-d, e1, e2);
-
-    float detA = glm::determinant(A);
-    float det = glm::determinant(mat3(b, e1, e2));
-    float t = det / detA;
-
-    if(t>=0 && t < closestIntersection.distance){
-      det = glm::determinant(mat3(-d, b, e2));
-      float u = det / detA;
-      if(u > 0 ){
-        det = glm::determinant(mat3(-d, e1, b));
-        float v = det/detA;
-        if(v >= 0 && (u + v) <= 1){
-          intersect = true;
-          closestIntersection.triangleIndex = i;
-          closestIntersection.distance = t;
-          closestIntersection.position = triangles[i].v0 + (u * vec4(e1.x,e1.y,e1.z, 0)) + (v * vec4(e2.x,e2.y,e2.z, 0));
-        }
+  float t = closestIntersection.distance;
+  vec4 position = vec4(0.f,0.f,0.f,1.f);
+  for(int i = 0; i < objects.size(); i++){
+    if(objects[i]->intersect(start, dir, t, position)){
+      if (t < closestIntersection.distance){
+        intersect = true;
+        closestIntersection.objectIndex = i;
+        closestIntersection.distance = t;
+        closestIntersection.position = position;
       }
     }
   }
   return intersect;
 }
 
-vector<vec4> RandLightPoints(){
-  vector<vec4> points;
+void RandLightPoints(vector<vec4> &points){
   if(options.num_light_rays == 1){
     points.push_back(light.position);
   }
@@ -170,37 +159,39 @@ vector<vec4> RandLightPoints(){
       points.push_back(vec4(x, light.position.y, z, 1.0));
     }
   }
-  return points;
 }
 
-vec3 DirectLight( const Intersection& i, const vector<Triangle>& triangles){
-  Triangle triangle = triangles[i.triangleIndex];
+vec3 DirectLight( const Intersection& i){
   vec3 D = vec3(0,0,0);
-  vector<vec4> points = RandLightPoints();
+  vector<vec4> points;
+  RandLightPoints(points);
   for(int x = 0; x < points.size(); x++){
-    vec4 r = normalize(points[x] - i.position);
+    glm::vec4 r = points[x] - i.position;
+    glm::vec3 r3 = glm::vec3(r.x, r.y, r.z);
+    r3 = glm::normalize(r3);
+    r = glm::vec4(r3.x, r3.y, r3.z, 1.f);
     float length_r = glm::length(points[x] - i.position);
 
     Intersection intersection;
     //in shadow
-    if (ClosestIntersection(i.position + 0.001f*r, r, triangles, intersection) && (length_r >= intersection.distance)){
+    if (ClosestIntersection(i.position + 0.001f*r, r, intersection) && (length_r >= intersection.distance)){
       //glass shadow
-      if (triangles[intersection.triangleIndex].material == Glass){
+      if (objects[intersection.objectIndex]->material == Glass){
         float A = 4*PI*length_r*length_r;
         vec3 B = light.color / A;
-
-        float C = glm::dot(r, triangle.normal);
+        vec4 normal = objects[intersection.objectIndex]->computeNormal( intersection.position );
+        float C = glm::dot(r, normal);
         C = glm::max(C, 0.f);
 
-        D += 0.8f * (B * C);
+        D += 0.3f * (B * C);
       }
       else D += vec3(0,0,0);
     }
     else{
       float A = 4*PI*length_r*length_r;
       vec3 B = light.color / A;
-
-      float C = glm::dot(r, triangle.normal);
+      vec4 normal = objects[i.objectIndex]->computeNormal( i.position );
+      float C = glm::dot(r, normal);
       C = glm::max(C, 0.f);
 
       D += B * C;
@@ -215,22 +206,30 @@ vec4 reflect(vec4 normal, vec4 dir){
 }
 
 vec4 refract (vec4 normal, vec4 dir, float& n, float& k){
-  float dot = glm::dot(normal, dir);
-  return n*dir + (n * dot - sqrt(k))*normal;;
+  float cosi = glm::clamp(-1.f,1.f,glm::dot(dir, normal));
+  float n1 = 1, n2 = 1.5;
+  if (cosi < 0) cosi = -cosi;
+  else {
+    std::swap(n1, n2);
+    normal = -normal;
+  }
+  n = n1 / n2;
+  k = 1 - n * n * (1 - cosi * cosi);
+  return (k < 0) ? vec4() : n * dir + (n * cosi - sqrtf(k)) * normal;
 }
 
 float fresnel(vec4 normal, vec4 dir, float& n, float& k){
   float kr = 0.0f;
-  float dot = glm::dot(normal, dir);
-  float n1 = 1.0f;
-  float n2 = 1.5f;
+  float dot = glm::clamp(1.f,1.f,glm::dot(normal, dir));
+  float n1 = 1.5f;
+  float n2 = 1.0f;
   if(dot > 0) std::swap(n1, n2);
   n = n1/n2;
   k = n * sqrt(std::max(0.f, 1-dot*dot));
   //total internal refrlection
   if(k >= 1) kr = 1;
   else{
-    float cost = sqrt(std::max(0.f, 1 - k * k));
+    float cost = sqrtf(std::max(0.f, 1 - k * k));
     dot = fabsf(dot);
     float Rs = ((n1 * dot) - (n2 * cost)) / ((n1 * dot) + (n2 * cost));
     float Rp = ((n2 * dot) - (n1 * cost)) / ((n2 * dot) + (n1 * cost));
@@ -239,54 +238,50 @@ float fresnel(vec4 normal, vec4 dir, float& n, float& k){
   return kr;
 }
 
-vec3 get_color(const Intersection& i, const vector<Triangle>& triangles, vec4 dir, int depth ){
-  Triangle triangle = triangles[i.triangleIndex];
-
-  if (triangle.material == Diff){
+vec3 get_color(const Intersection& i, vec4 dir, int depth ){
+  if (objects[i.objectIndex]->material == Diff){
     vec3 indirectLight = 0.5f*vec3( 1, 1, 1 );
-    vec3 directLight = DirectLight(i, triangles);
-    vec3 totalLight = directLight + indirectLight;
-    return triangle.color * totalLight;
+    vec3 directLight = DirectLight(i);
+    return objects[i.objectIndex]->color * (directLight + indirectLight);
   }
 
-  else if((triangle.material == Spec) && (depth < options.max_depth)){
+  else if((objects[i.objectIndex]->material == Spec) && (depth < options.max_depth)){
     //reflect all light
-    vec4 new_dir = reflect(triangle.normal, dir);
+    vec4 new_dir = reflect(objects[i.objectIndex]->computeNormal( i.position ), dir);
     Intersection closestIntersection;
-    if(ClosestIntersection(i.position+(0.001f*new_dir), new_dir, triangles, closestIntersection)){
-      return 0.8f * get_color(closestIntersection, triangles, new_dir, depth + 1);
+    if(ClosestIntersection(i.position+(0.001f*new_dir), new_dir, closestIntersection)){
+      return 0.8f * get_color(closestIntersection, new_dir, depth + 1);
     }
     else return vec3(0,0,0);
   }
 
-  else if (triangle.material == Glass && (depth < options.max_depth)){
+  else if (objects[i.objectIndex]->material == Glass && (depth < options.max_depth)){
     // refract/reflect
     float n = 0.0f;
     float k = 0.0f;
-    float kr = fresnel(triangle.normal, dir, n, k);
+    vec4 normal = objects[i.objectIndex]->computeNormal( i.position );
+    float kr = fresnel(normal, dir, n, k);
     vec3 refract_color;
     if(kr < 1){
-      vec4 refract_dir = refract(triangle.normal, dir, n, k);
+      vec4 refract_dir = refract(normal, dir, n, k);
       Intersection closestIntersection;
-      if(ClosestIntersection(i.position+(0.001f*refract_dir), refract_dir, triangles, closestIntersection)){
-        refract_color = 0.9f * get_color(closestIntersection, triangles, refract_dir, depth+1);
+      if(ClosestIntersection(i.position+(0.0001f*refract_dir), refract_dir, closestIntersection)){
+        refract_color = 0.8f * get_color(closestIntersection, refract_dir, depth+1);
       }
       else refract_color = vec3(0,0,0);
     }
-    vec4 reflect_dir = reflect(triangle.normal, dir);
-    vec3 reflect_color;
+    vec4 reflect_dir = reflect(normal, dir);
+    vec3 reflect_color = vec3(0,0,0);
     Intersection intersection;
-    if(ClosestIntersection(i.position+(0.001f*reflect_dir), reflect_dir, triangles, intersection)){
-      reflect_color = 0.8f * get_color(intersection, triangles, reflect_dir, depth + 1);
+    if(ClosestIntersection(i.position+(0.0001f*reflect_dir), reflect_dir, intersection)){
+      reflect_color = 0.8f * get_color(intersection, reflect_dir, depth + 1);
     }
-    else reflect_color = vec3(0,0,0);
     return (reflect_color * kr) + (refract_color*(1-kr));
   }
   else return vec3(0,0,0);
 }
 
-vector<vec4> RandCameraPoints(){
-  vector<vec4> points;
+void RandCameraPoints(vector<vec4> &points){
   if(options.num_dof_rays == 1){
     points.push_back(camera.position);
   }
@@ -298,13 +293,13 @@ vector<vec4> RandCameraPoints(){
       points.push_back(vec4(x, y, z, 1.0));
     }
   }
-  return points;
 }
 
-void Draw(screen* screen, const vector<Triangle>& triangles){
+void Draw(screen* screen){
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  vector<vec4> points = RandCameraPoints();
+  vector<vec4> points;
+  RandCameraPoints(points);
 
   for(int x = 0; x<SCREEN_WIDTH; x++){
     for(int y = 0; y<SCREEN_HEIGHT; y++){
@@ -315,8 +310,8 @@ void Draw(screen* screen, const vector<Triangle>& triangles){
         vec4 ray_intersection = points[i] + (dir * camera.dof_length);
         dir = ray_intersection - points[i];
 
-        if (ClosestIntersection(points[i], dir, triangles, closestIntersection)){
-          color += get_color(closestIntersection, triangles, dir, 0);
+        if (ClosestIntersection(points[i], dir, closestIntersection)){
+          color += get_color(closestIntersection, dir, 0);
         }
       }
       float scale = 1.f/options.num_dof_rays;
@@ -332,10 +327,7 @@ void Update(){
   float dt = float(t2-t);
   t = t2;
   float st = dt/1000;
-  /*Good idea to remove this*/
-  // std::cout << "Render time: " << st << " s." << std::endl;
-
-  printf("Aperture = %f \n DoF Length = %f\n\n", camera.aperture, camera.dof_length);
+  std::cout << "Render time: " << st << " s." << std::endl;
 
   //camera movement
   const Uint8 *keystate = SDL_GetKeyboardState( 0 );
@@ -352,10 +344,10 @@ void Update(){
     camera.position.x += 0.1;
   }
   if (keystate[SDL_SCANCODE_COMMA]){
-    camera.angle -= 0.02;
+    camera.angle += 0.02;
   }
   if( keystate[SDL_SCANCODE_PERIOD]){
-    camera.angle += 0.02;
+    camera.angle -= 0.02;
   }
 
   if( keystate[SDL_SCANCODE_L] ){
